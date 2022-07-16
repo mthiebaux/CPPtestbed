c10aef9536f2bf937831eb0ee5734e225d538487
mthiebaux <thiebaux@gmail.com>
2022-07-16 16:00:00 -0400

    Update graph.cpp

--- src/algos/graph.cpp
+++ src/algos/graph.cpp
@@ -20,6 +20,8 @@
 
 using namespace std;
 
+// INFINITY: double inf = std::numeric_limits<double>::infinity();
+
 /////////////////////////////////////////////////////////////
 
 class Node	{
@@ -33,13 +35,19 @@ class Node	{
 		~Node()	{
 		}
 
-		// pass by value or reference: see #6: https://www.py4u.net/discuss/64291
-		void connect( const shared_ptr< Node >& snp )	{ // faster
+		// https://stackoverflow.com/questions/3310737/should-we-pass-a-shared-ptr-by-reference-or-by-value
+		// 	pass by const reference
+
+		void connect( const shared_ptr< Node >& snp )	{
+
 			if( snp->identity != identity )	{ // self check, no err?
+
 				edge_list.push_front( snp );
+//				edge_list.push_front( make_pair( snp, 1.0 ) );
 			}
 		}
-		void disconnect( const shared_ptr< Node >& rem )	{
+
+		void disconnect( const shared_ptr< Node >& rem )	{ // breaks connected components
 
 			for( auto& np: edge_list )	{
 				if( np == rem )	{
@@ -51,9 +59,11 @@ class Node	{
 				}
 			}
 		}
-		bool edge_connected( int id )	{
+
+		bool adjacent( int id )	{
 
 			for( auto& snp: edge_list )	{
+
 				if( snp->identity == id )	{
 					return( true );
 				}
@@ -62,59 +72,83 @@ class Node	{
 		}
 
 		int identity;
-		int root_identity; // connectivity id
+		int root_identity; // connected component id
+
 		bool visited;
+
 		forward_list< const shared_ptr< Node > > edge_list;
+//		forward_list< const shared_ptr< pair< Node, double > > > edge_list;
 };
 
 /////////////////////////////////////////////////////////////
 
-// Undirected Cyclic Graph
-
-// class UndirectedGraph
-class graph	{
+class CyclicGraph	{
 
 	public:
-		graph()	{
+
+		CyclicGraph()	{
 		}
-		~graph()	{
+		~CyclicGraph()	{
 		}
+
+#if 1
+		map< int, shared_ptr< Node > > // ordered for print out
+			node_map; // Fast range iter. Slow [key]. Search, removal, insertion: logarithmic complexity.
+#else
+		unordered_map< int, shared_ptr< Node > >
+			node_map; // Slow range iter. Fast [key]. Search, removal, insertion: average constant-time complexity.
+#endif
+
 		bool insert( Node* new_node_p )	{
 
 			if( node_map.count( new_node_p->identity ) != 0 )	{ // duplicate insertion check/err
-				cout << "graph::insert( " << new_node_p->identity << " ) DUPLICATE" << endl;
+				cout << "CyclicGraph::insert( " << new_node_p->identity << " ) DUPLICATE" << endl;
 				// delete?
 				return( false );
 			}
 
 			node_map.insert( make_pair( new_node_p->identity, shared_ptr< Node >( new_node_p ) ) );
-//			node_map.insert( make_pair( new_node_p->identity, node_map[ new_node_p->identity ] ) ); // not shared
 			return( true );
 		}
+
 		bool insert( int id )	{
 			return( insert( new Node( id ) ) );
 		}
+
+		// bidirectional
 		void connect( int a, int b )	{
 
 			if( a == b )	{ // self connect check err?
 				return;
 			}
-			if( node_map[ a ]->edge_connected( b ) )	{ // duplicate connect
+			if( node_map[ a ]->adjacent( b ) )	{ // duplicate connect
 				return;
 			}
 			node_map[ a ]->connect( node_map[ b ] );
 			node_map[ b ]->connect( node_map[ a ] );
 
-			int root_a_id = find_root( node_map[ a ].get() );
+			int root_a_id = find_root( node_map[ a ].get() ); // move to calc_components iteration?
 			int root_b_id = find_root( node_map[ b ].get() );
 			node_map[ root_a_id ]->root_identity = root_b_id;
 		}
+
 		void reset_roots( void )	{
 			for( auto& np: node_map )	{
 				np.second->root_identity = np.second->identity;
 			}
 		}
+
+		void reset_root( int root_id )	{ // only specified component
+			for( auto& np: node_map )	{
+				if( np.second->root_identity == root_id )	{
+					np.second->root_identity = np.second->identity;
+				}
+			}
+		}
+
 #if 1
+		// convert recursive to iterative...
+
 		int find_root( Node* node_p )	{
 
 			if( node_p->root_identity == node_p->identity )	{
@@ -125,6 +159,7 @@ class graph	{
 			);
 			return( node_p->root_identity );
 		}
+
 		int find_root( int id )	{
 			return( find_root( node_map[ id ].get() ) );
 		}
@@ -139,6 +174,7 @@ class graph	{
 		bool connected( int a, int b )	{ // iteration: DFS with std::stack
 
 			reset_visits();
+
 			stack< Node* > dfs_stack;
 			dfs_stack.push( node_map[ a ].get() ); // use raw pointers locally
 
@@ -153,6 +189,7 @@ class graph	{
 					return( true );
 				if( find_root( target_node_p ) == find_root( node_p ) )
 					return( true );
+
 				node_p->visited = true;
 
 				for( auto& snp: node_p->edge_list )	{
@@ -167,6 +204,7 @@ class graph	{
 		bool connected( int a, int b )	{ // iteration: BFS with std::list OR std::queue
 
 			reset_visits();
+
 			list< Node* > bfs_list;
 			bfs_list.push_back( node_map[ a ].get() );
 
@@ -181,6 +219,7 @@ class graph	{
 					return( true );
 				if( find_root( target_node_p ) == find_root( node_p ) )
 					return( true );
+
 				node_p->visited = true;
 
 				for( auto& snp: node_p->edge_list )	{
@@ -192,6 +231,7 @@ class graph	{
 			return( false );
 		}
 #endif
+
 		void remove( int id )	{
 
 			shared_ptr< Node > remp = node_map[ id ];
@@ -200,16 +240,20 @@ class graph	{
 				remp->disconnect( np.second );
 				np.second->disconnect( remp );
 
-				// needs to reset root_identity... traverse entire map for same root, set each to self
+				// needs to reset root_identity...
+				// traverse entire map for same root, set each to self
 			}
 			node_map.erase( id );
 		}
+
+////////////////////////////
 
 		char alphanumeric_filter( int i )	{
 			static string ANF = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ*";
 			size_t len = ANF.size();
 			return( ANF[ i % len ] );
 		}
+
 		void print_nodes( void )	{
 
 			for( auto& np: node_map )	{
@@ -233,6 +277,7 @@ class graph	{
 				cout << " }" << endl;
 			}
 		}
+
 		void print_paths( void )	{
 
 			size_t size = node_map.size();
@@ -266,7 +311,7 @@ class graph	{
 					else
 					if( connected( np.second->identity, id_vec[ c ] ) )	{
 #if 0
-						if( np.second->edge_connected( id_vec[ c ] ) )	{
+						if( np.second->adjacent( id_vec[ c ] ) )	{
 							cout << "O";
 						}
 						else	{
@@ -283,21 +328,65 @@ class graph	{
 				cout << endl;
 			}
 		}
-
-#if 1
-		map< int, shared_ptr< Node > > // ordered for print out
-			node_map; // Fast range iter. Slow [key]. Search, removal, insertion: logarithmic complexity.
-#else
-		unordered_map< int, shared_ptr< Node > >
-			node_map; // Slow range iter. Fast [key]. Search, removal, insertion: average constant-time complexity.
-#endif
 };
 
 /////////////////////////////////////////////////////////////
 
-#if 1
+void test_graph_node_class()	{
 
-void test_graph_node_perf( int size, double prob, int reps = -1, bool print_out = false )	{
+	CyclicGraph G;
+
+	G.insert( 0 );
+	G.insert( 1 );
+	G.insert( 2 );
+	G.insert( 3 );
+	G.insert( 4 );
+	G.insert( 5 );
+
+	G.connect( 0, 5 );
+	G.connect( 1, 2 );
+	G.connect( 4, 3 );
+	G.connect( 3, 5 );
+//	G.connect( 5, 1 );
+
+	G.print_nodes();
+	cout << "==" << endl;
+
+	G.print_paths();
+	cout << "--" << endl;
+
+#if 0
+//	G.find_root( 0 );
+	G.find_root( 1 );
+//	G.find_root( 2 );
+#endif
+
+	G.print_nodes();
+	cout << "--" << endl;
+
+#if 0
+	G.remove( 1 );
+	G.print_paths();
+	cout << "--" << endl;
+
+//	G.insert( new Node( 1 ) );
+	G.insert( 1 );
+	G.print_paths();
+	cout << "--" << endl;
+
+	G.connect( 0, 1 );
+	G.print_paths();
+	cout << "--" << endl;
+
+	G.connect( 1, 2 );
+	G.print_paths();
+	cout << "--" << endl;
+#endif
+}
+
+/////////////////////////////////////////////////////////////
+
+void test_graph_perf( int size, double prob, int reps = -1, bool print_out = false )	{
 
 	cout << "TEST: test_graph_node_perf: " << size << " prob: " << prob << " reps: " << reps << endl;
 	Timer T_full, T_part;
@@ -309,7 +398,7 @@ void test_graph_node_perf( int size, double prob, int reps = -1, bool print_out 
 	cout << "Construction:" << endl;
 	T_part.reset();
 
-	graph G;
+	CyclicGraph G;
 	vector< int > id_vec; // This is the id protection.
 	for( int i=0; i<size; i++ )	{
 //		G.insert( new Node( i ) );
@@ -412,174 +501,25 @@ void test_graph_node_perf( int size, double prob, int reps = -1, bool print_out 
 	}
 }
 
-#endif
+int test_graph_perf_scenarios( void ) {
 
-/////////////////////////////////////////////////////////////
-
-void test_graph_node_class()	{
-
-	graph G;
-#if 0
-	G.insert( new Node( 0 ) );
-	G.insert( new Node( 1 ) );
-	G.insert( new Node( 2 ) );
-	G.insert( new Node( 3 ) );
-	G.insert( new Node( 4 ) );
-	G.insert( new Node( 5 ) );
-#else
-	G.insert( 0 );
-	G.insert( 1 );
-	G.insert( 2 );
-	G.insert( 3 );
-	G.insert( 4 );
-	G.insert( 5 );
-#endif
-
-	G.connect( 0, 5 );
-	G.connect( 1, 2 );
-	G.connect( 4, 3 );
-	G.connect( 3, 5 );
-	G.connect( 5, 1 );
-
-	G.print_nodes();
-	cout << "==" << endl;
-
-	G.print_paths();
-	cout << "--" << endl;
-
-#if 0
-//	G.find_root( 0 );
-	G.find_root( 1 );
-//	G.find_root( 2 );
-#endif
-
-	G.print_nodes();
-	cout << "--" << endl;
-
-#if 0
-	G.remove( 1 );
-	G.print_paths();
-	cout << "--" << endl;
-
-//	G.insert( new Node( 1 ) );
-	G.insert( 1 );
-	G.print_paths();
-	cout << "--" << endl;
-
-	G.connect( 0, 1 );
-	G.print_paths();
-	cout << "--" << endl;
-
-	G.connect( 1, 2 );
-	G.print_paths();
-	cout << "--" << endl;
-#endif
-
-}
-
-/////////////////////////////////////////////////////////////
-
-#if 1
-
-int graph_perf_scenarios( void ) {
-
-#if 1
 	double p = 0.65;
 
-		test_graph_node_perf( 10, p, -1, true );
-//		test_graph_node_perf( 50, p, -1, true );
+		test_graph_perf( 10, p, -1, true );
+//		test_graph_perf( 50, p, -1, true );
 
-	// sys-stack recursion on node:
-
-//		test_graph_node_perf( 10000, p, 10000 );  // 1.62
-//		test_graph_node_perf( 20000, p, 10000 );  // 3.6
-//		test_graph_node_perf( 40000, p, 10000 );  // 12.2
-//		test_graph_node_perf( 80 * 1000, p, 10000 );  // 25.7
-//		test_graph_node_perf( 160 * 1000, p, 10000 );  // 57.0
-//		test_graph_node_perf( 320 * 1000, p, 10000 );  // 115.2
-//		test_graph_node_perf( 640 * 1000, p, 10000 );  // 461
-//		test_graph_node_perf( 1000 * 1000, p, 10000 );  // 697 (11.6 min)
-//		test_graph_node_perf( 1280 * 1000, p, 10000 );  // stack blown
-
-p = 0.6;
-//		test_graph_node_perf( 10000, p, 10000 );  // 0.77
-//		test_graph_node_perf( 20 * 1000, p, 10000 );  // 1.6
-//		test_graph_node_perf( 40 * 1000, p, 10000 );  // 5.2
-//		test_graph_node_perf( 80 * 1000, p, 10000 );  // 14.5
-//		test_graph_node_perf( 160 * 1000, p, 10000 );  // 33.5
-//		test_graph_node_perf( 320 * 1000, p, 10000 );  // 71.5
-
-	// DFS:
-p = 0.8;
-//		test_graph_node_perf( 1000 * 1000, p, 10000 );  // (est 10 min) 642 (10.7 min)
-//		test_graph_node_perf( 2000 * 1000, p, 10000 );  // (est 20 min) 1484 (25 min)
-
-
-p = 0.6;
-//		test_graph_node_perf( 10000, p, 10000 );  // 0.79
-//		test_graph_node_perf( 20000, p, 10000 );  // 1.7
-//		test_graph_node_perf( 40000, p, 10000 );  // 5.5
-//		test_graph_node_perf( 80000, p, 10000 );  // 12.2
-//		test_graph_node_perf( 160 * 1000, p, 10000 );  // 29.8
-//		test_graph_node_perf( 320 * 1000, p, 10000 );  // 66.7
-//		test_graph_node_perf( 640 * 1000, p, 10000 );  // 99.6
-//		test_graph_node_perf( 1280 * 1000, p, 10000 );  // 454.0 (7.6 min)
-//		test_graph_node_perf( 2560 * 1000, p, 10000 );  // 656.0 (11 min)
-//		test_graph_node_perf( 2560 * 1000, p, 100 );  // 7.3
-//		test_graph_node_perf( 5120 * 1000, p, 100 );  // 13.9
-//		test_graph_node_perf( 10 * 1000 * 1000, p, 100 );  // 31.1
-//		test_graph_node_perf( 20 * 1000 * 1000, p, 100 );  // 56.1
-//		test_graph_node_perf( 40 * 1000 * 1000, p, 100 );  // 157.6
-
-//		test_graph_node_perf( 100 * 1000, p, 1000 );  // 3.25
-//		test_graph_node_perf( 200 * 1000, p, 1000 );  // 8.28
-//		test_graph_node_perf( 400 * 1000, p, 1000 );  // 18.2
-//		test_graph_node_perf( 800 * 1000, p, 1000 );  // 64.0
-
-p = 0.8;
-//		test_graph_node_perf( 400 * 1000, p, 100 );  // 1.63
-//		test_graph_node_perf( 800 * 1000, p, 100 );  // 4.65
-//		test_graph_node_perf( 1600 * 1000, p, 100 );  // 13.9
-//		test_graph_node_perf( 3200 * 1000, p, 100 );  // 24.2
-
-//		test_graph_node_perf( 5 * 1000 * 1000, p, 10 );  // 3.8 : 0.25 : 5.0
-//		test_graph_node_perf( 10 * 1000 * 1000, p, 10 ); // 8.1 : 0.5 : 9.2
-//		test_graph_node_perf( 20 * 1000 * 1000, p, 10 ); // 16.1 : 1.0 : 12.5
-//		test_graph_node_perf( 40 * 1000 * 1000, p, 10 ); // 32.5 : 2.7 : 29.4
-//		test_graph_node_perf( 80 * 1000 * 1000, p, 10 ); // 76.4 : 20.2 : 157.7
-
-	// BFS:
-
-//		test_graph_node_perf( 100 * 1000, p, 100 ); // 0.05 : 0.004 : 1.25
-//		test_graph_node_perf( 200 * 1000, p, 100 ); // 2.95
-//		test_graph_node_perf( 400 * 1000, p, 100 ); // 8.04
-//		test_graph_node_perf( 800 * 1000, p, 100 ); // 16.0
-//		test_graph_node_perf( 1600 * 1000, p, 100 ); // 31.1
-//		test_graph_node_perf( 3200 * 1000, p, 100 ); // 70.5
-
-//		test_graph_node_perf( 1 * 1000 * 1000, p, 10 ); // 0.72 : 0.04 : 1.9
-//		test_graph_node_perf( 2 * 1000 * 1000, p, 10 ); // 1.5 : 0.1 : 2.7
-//		test_graph_node_perf( 4 * 1000 * 1000, p, 10 ); // 3.0 : 0.2 : 10.9
-//		test_graph_node_perf( 5 * 1000 * 1000, p, 10 ); // 3.7 : 0.23 : 4.4
-//		test_graph_node_perf( 10 * 1000 * 1000, p, 10 ); // 7.8 : 0.47 : 30.2
-
-p = 1.0;
-
-//		test_graph_node_perf( 1 * 1000 * 1000, p, 20 ); 		// 2.4
-//		test_graph_node_perf( 2 * 1000 * 1000, p, 20 ); 		// 4.5
-//		test_graph_node_perf( 4 * 1000 * 1000, p, 20 ); 		// 8.6
-//		test_graph_node_perf( 8 * 1000 * 1000, p, 20 ); 		// 16.0
-//		test_graph_node_perf( 16 * 1000 * 1000, p, 20 ); 		// 44.2
-//		test_graph_node_perf( 32 * 1000 * 1000, p, 20 ); 		// 97.7
-//		test_graph_node_perf( 64 * 1000 * 1000, p, 20 ); 		// 59.2 : 297.4
-//		test_graph_node_perf( 128 * 1000 * 1000, p, 20 ); 	// 2020.86 (33.7 min) : (2.5 hrs? KILLED)
-
-#endif
+//		test_graph_perf( 10000, p, 10000 );  // 1.62
+//		test_graph_perf( 20000, p, 10000 );  // 3.6
+//		test_graph_perf( 40000, p, 10000 );  // 12.2
+//		test_graph_perf( 80 * 1000, p, 10000 );  // 25.7
+//		test_graph_perf( 160 * 1000, p, 10000 );  // 57.0
+//		test_graph_perf( 320 * 1000, p, 10000 );  // 115.2
+//		test_graph_perf( 640 * 1000, p, 10000 );  // 461
+//		test_graph_perf( 1000 * 1000, p, 10000 );  // 697 (11.6 min)
+//		test_graph_perf( 1280 * 1000, p, 10000 );  // stack blown
 
     return 0;
 }
-
-#endif
 
 /////////////////////////////////////////////////////////////
 
@@ -587,6 +527,6 @@ void test_graph_algorithms( void )	{
 
 	test_graph_node_class();
 
-
+//	test_graph_perf_scenarios();
 }
 
